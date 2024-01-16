0d831bf2dbdd229a45faf37ef6ed904f0db0d30e
AA <128122688+soulqge@users.noreply.github.com>
2024-01-16 09:30:43 +0700

    konversi_suhu
    initial

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:belajar_flu/services.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Konversi Suhu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const servicesWidget(),
+    );
+  }
+}
